47ad599041f5306c2083c085aa098120bd34f0f2
Karl Bowden <karl@karlbowden.com>
2015-12-20 13:56:53 +1100

    Export and test createStore, createStateStream and createDisposable
    Signed-off-by: Karl Bowden <karl@karlbowden.com>

--- ReduxBond/ReduxBond.swift
+++ ReduxBond/ReduxBond.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2015 SwiftRedux. All rights reserved.
 //
 
-import Foundation
+import Bond
+import Redux
+
+
+/**
+
+ Uses `createStateStream` to create a `Redux.Store<State>` using a
+ `Bond.Observable<State>` stream.
+
+ */
+public func createStore<State>(
+    reducer: ((State?, Action) -> State),
+    state: State? = nil)
+    -> Store<State> {
+
+        return createStreamStore(createStream, reducer: reducer, state: state)
+}
+
+
+/**
+
+ Accepts a `State` and returns `Redux.StateStream<State>` using a
+ `Bond.Observable<State>` as the stream provider.
+
+ */
+public func createStream<State>(state: State) -> StateStream<State> {
+
+    let observable = Observable(state)
+
+    return StateStream(
+        dispatch: observable.next,
+        subscribe: { createDisposable(observable.observe($0)) },
+        getState: { observable.value })
+}
+
+
+/**
+
+ Accepts a `Bond.DisposableType` and returns it wrapped as a `ReduxDisposable`.
+
+ */
+public func createDisposable(disposable: DisposableType) -> ReduxDisposable {
+
+    return SimpleReduxDisposable(disposed: { disposable.isDisposed }, dispose: disposable.dispose)
+}
--- ReduxBondTests/ReduxBondTests.swift
+++ ReduxBondTests/ReduxBondTests.swift
@@ -7,30 +7,96 @@
 //
 
 import XCTest
+import Bond
+import Redux
 @testable import ReduxBond
 
 class ReduxBondTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    let store: Store<State> = ReduxBond.createStore(reducer)
+    let action = IncrementAction()
+    let action2 = IncrementAction(payload: 2)
+
+    func testStoreCreation() {
+        // Assert
+        XCTAssert(store.state.count == 0)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testStoreActions() {
+        // Act
+        store.dispatch(action)
+
+        // Assert
+        XCTAssert(store.state.count == 1)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testStoreSubscription() {
+        // Arrange
+        var state: State!
+        store.subscribe { state = $0 }
+
+        // Act
+        store.dispatch(action)
+
+        // Assert
+        XCTAssert(state.count == 1)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
+
+    func testSubscribeInvokesCallbackImmediately() {
+        // Act
+        var state: State!
+        store.subscribe { state = $0 }
+
+        // Assert
+        XCTAssert(state.count == 0)
+    }
+
+    func testMultipleActions() {
+        // Arrange
+        var state: State!
+        store.subscribe { state = $0 }
+
+        // Act
+        store.dispatch(action)
+        store.dispatch(action2)
+
+        // Assert
+        XCTAssert(state.count == 3)
+    }
+
+    func testDisposability() {
+        // Arrange
+        var state: State!
+        var disposedState: State!
+        store.subscribe { state = $0 }
+        let disposable = store.subscribe { disposedState = $0 }
+
+        // Act
+        store.dispatch(action)
+        disposable.dispose()
+        store.dispatch(action)
+
+        // Assert
+        XCTAssert(state.count == 2)
+        XCTAssert(disposedState.count == 1)
+    }
+
+    func testPerformance() {
+        let count = 100000
         self.measureBlock {
-            // Put the code you want to measure the time of here.
+            // Arrange
+            let store = ReduxBond.createStore(reducer)
+            let action = IncrementAction()
+            var state: State!
+            store.subscribe { state = $0 }
+
+            // Act
+            for _ in 1...count {
+                store.dispatch(action)
+            }
+
+            // Assert
+            XCTAssert(state.count == count)
         }
     }
-    
 }
