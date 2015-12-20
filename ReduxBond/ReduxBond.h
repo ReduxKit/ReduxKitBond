228c0821721f73e987d002179e2156e2c29bd408
Karl Bowden <karl@karlbowden.com>
2015-12-20 13:26:12 +1100

    Initial Commit

---
+++ ReduxBond/ReduxBond.h
@@ -0,0 +1,19 @@
+//
+//  ReduxBond.h
+//  ReduxBond
+//
+//  Created by Karl Bowden on 20/12/2015.
+//  Copyright © 2015 SwiftRedux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReduxBond.
+FOUNDATION_EXPORT double ReduxBondVersionNumber;
+
+//! Project version string for ReduxBond.
+FOUNDATION_EXPORT const unsigned char ReduxBondVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReduxBond/PublicHeader.h>
+
+
